be96328232dc1ca5a68f8a3619ed26d90f6ec940
alefunt <sashkak0204@gmail.com>
2021-12-06 20:33:47 +0300

    HOTFIXjbcimpl

--- src/main/java/com/audiostock/entities/Request.java
+++ src/main/java/com/audiostock/entities/Request.java
@@ -7,6 +7,7 @@ import java.util.Date;
 
 @Entity
 public class Request {
+
     @Id
     private Long id;
 
@@ -20,7 +21,75 @@ public class Request {
     private User moderator;
 
     private boolean solution;
+
     private String rejectionReason;
     private Date creationDate;
     private Date reviewDate;
+
+    public Request() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public Track getTrack() {
+        return track;
+    }
+
+    public void setTrack(Track track) {
+        this.track = track;
+    }
+
+    public User getModerator() {
+        return moderator;
+    }
+
+    public void setModerator(User moderator) {
+        this.moderator = moderator;
+    }
+
+    public boolean isSolution() {
+        return solution;
+    }
+
+    public void setSolution(boolean solution) {
+        this.solution = solution;
+    }
+
+    public String getRejectionReason() {
+        return rejectionReason;
+    }
+
+    public void setRejectionReason(String rejectionReason) {
+        this.rejectionReason = rejectionReason;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getReviewDate() {
+        return reviewDate;
+    }
+
+    public void setReviewDate(Date reviewDate) {
+        this.reviewDate = reviewDate;
+    }
 }
