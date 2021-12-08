22c67b8efff39fba08fb9d755977972ffe3f5999
clif2501 <clif2501@gmail.com>
2021-12-08 21:45:22 +0300

    StatusRepo extends CrudRepository instead of PagingAndSortingRepository

--- src/main/java/com/audiostock/repos/StatusRepo.java
+++ src/main/java/com/audiostock/repos/StatusRepo.java
@@ -1,7 +1,8 @@
 package com.audiostock.repos;
 
 import com.audiostock.entities.Status;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface StatusRepo extends PagingAndSortingRepository<Status, Long> {
+public interface StatusRepo extends CrudRepository<Status, Long> {
 }
