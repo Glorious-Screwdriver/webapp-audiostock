140a6924b7551e5bb21c0ca72d503bcb5e98c595
alefunt <sashkak0204@gmail.com>
2021-12-26 21:45:19 +0300

    nemnogo ui

--- src/main/java/com/audiostock/controller/HelloController.java
+++ src/main/java/com/audiostock/controller/HelloController.java
@@ -2,13 +2,25 @@ package com.audiostock.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloController {
 
-    @GetMapping(value = "/hello")
-    public String hello() {
-        return "hello";
+    @GetMapping("*")
+    public String index(){
+        return "catal";
     }
-
+    @GetMapping(value = "/login")
+    public String hello() {
+        System.err.println("login get");
+        return "login";
+    }
+    @PostMapping("/login")
+    public void login(HttpServletRequest request){
+        System.out.println(request.getRequestURL());
+        System.err.println("login post");
+    }
 }
--- src/main/webapp/WEB-INF/jsp/hello.jsp
+++ src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -1,10 +1,3 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: Viacheslav
-  Date: 26.12.2021
-  Time: 14:51
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
