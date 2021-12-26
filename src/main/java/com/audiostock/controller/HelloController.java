8b5ebc9743a943d666f307c2c2bf65efebc05485
clif2501 <clif2501@gmail.com>
2021-12-27 02:06:08 +0300

    Converting to Thymeleaf

--- src/main/java/com/audiostock/controller/HelloController.java
+++ src/main/java/com/audiostock/controller/HelloController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 import java.util.Optional;
 
@@ -34,27 +33,21 @@ public class HelloController {
     }
 
     @PostMapping("/register")
-    public String postReg(@RequestParam String login,
-                          @RequestParam String password,
-                          @RequestParam String repeat,
-                          Map<String, Object> model) {
-        System.err.println("it started");
+    public String postReg(String login, String password, String repeat, Map<String, Object> model) {
+        System.err.println("postReg");
+
         if (!password.equals(repeat)) {
-            System.err.println("don't match");
             model.put("message", "Passwords don't match");
             return "register";
         }
 
         final Optional<User> userWithSameName = userRepo.findByLogin(login);
         if (userWithSameName.isEmpty()) {
-            System.err.println("already taken");
             model.put("message", "Login is already taken");
             return "register";
         }
 
         userRepo.save(new User(login, password));
-
-        System.err.println("postReg");
 
         return "redirect:/";
     }
