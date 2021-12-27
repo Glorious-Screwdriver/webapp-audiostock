cebad18af1c1f2a3df6fa33196fcf326a34f88a0
clif2501 <clif2501@gmail.com>
2021-12-27 21:09:38 +0300

    CartController done

---
+++ src/main/java/com/audiostock/controller/CartController.java
@@ -0,0 +1,70 @@
+package com.audiostock.controller;
+
+import com.audiostock.entities.Track;
+import com.audiostock.entities.User;
+import com.audiostock.service.UserService;
+import com.audiostock.service.exceptions.UserNotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.security.Principal;
+import java.util.List;
+
+@Controller("/cart")
+public class CartController {
+
+    UserService userService;
+
+    public CartController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public String cart(Principal principal) {
+        User user = getUserFromPrincipal(principal);
+
+        List<Track> cart = userService.getCartSortedByName(user);
+        Long totalCost = userService.totalCartPrice(user);
+
+        //TODO /cart view
+        throw new UnsupportedOperationException();
+    }
+
+    @GetMapping("/checkout")
+    public String getCheckout(Principal principal) {
+        User user = getUserFromPrincipal(principal);
+
+        long balance = user.getBalance();
+        long totalCost = userService.totalCartPrice(user);
+
+        //TODO /cart/checkout view
+        throw new UnsupportedOperationException();
+    }
+
+    @PostMapping("/checkout")
+    public String postCheckout(Principal principal) {
+        User user = getUserFromPrincipal(principal);
+
+        final boolean checkoutSucceeded = userService.checkout(user);
+
+        if (checkoutSucceeded) {
+            //TODO /purchased view
+            throw new UnsupportedOperationException("/purchased view is not supported");
+        } else {
+            //TODO /deposit view
+            throw new UnsupportedOperationException("/deposit view is not supported");
+        }
+    }
+
+    private User getUserFromPrincipal(Principal principal) {
+        User user;
+        try {
+            user = userService.getUserByUsername(principal.getName());
+        } catch (UserNotFoundException e) {
+            throw new IllegalStateException(e);
+        }
+        return user;
+    }
+
+}
