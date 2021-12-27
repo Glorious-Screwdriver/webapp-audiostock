2b9ee20ee2bacf8efee792a5c950785100fd2780
clif2501 <clif2501@gmail.com>
2021-12-27 21:09:30 +0300

    FavoriteController done

---
+++ src/main/java/com/audiostock/controller/FavoriteController.java
@@ -0,0 +1,41 @@
+package com.audiostock.controller;
+
+import com.audiostock.entities.Track;
+import com.audiostock.entities.User;
+import com.audiostock.service.UserService;
+import com.audiostock.service.exceptions.UserNotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+import java.util.List;
+
+@Controller("/favorite")
+public class FavoriteController {
+
+    UserService userService;
+
+    public FavoriteController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public String favorite(Principal principal) {
+        User user = getUserFromPrincipal(principal);
+        List<Track> favorites = userService.getFavoriteSortedByName(user);
+
+        //TODO /favorite view
+        throw new UnsupportedOperationException();
+    }
+
+    private User getUserFromPrincipal(Principal principal) {
+        User user;
+        try {
+            user = userService.getUserByUsername(principal.getName());
+        } catch (UserNotFoundException e) {
+            throw new IllegalStateException(e);
+        }
+        return user;
+    }
+
+}
