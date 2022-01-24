4d870ed79bf2a3356c20f7a452764d691b42da10
clif2501 <clif2501@gmail.com>
2022-01-24 16:46:12 +0300

    Dynamic content served correctly (resolves #68)

--- src/main/java/com/audiostock/WymApplication.java
+++ src/main/java/com/audiostock/WymApplication.java
@@ -3,10 +3,16 @@ package com.audiostock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+import java.io.IOException;
+
 @SpringBootApplication
 public class WymApplication {
 
-	public static void main(String[] args) {
+	public static String dataDir;
+
+	public static void main(String[] args) throws IOException {
+		dataDir = new File(".").getCanonicalPath() + "\\data\\";
 		SpringApplication.run(WymApplication.class, args);
 	}
 
